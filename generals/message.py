bc144c4abc58840fcb628f69887ee7f5dc6dbff9
Pham Le Hai Au <mycrystalheart2000@yahoo.com>
2018-10-09 06:04:40 +0700

    Delete message.py

--- generals/message.py
+++
@@ -1,6 +0,0 @@
-# Custom Message
-verifytitle = "Current Page title is "
-verifyurl = "Current URL is "
-verifyenable = "Element is not enabled"
-verifydisable = "Element is not disabled"
-verifyselected = "Element is not selected"
